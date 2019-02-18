aaa53fa903fa1d088ad05aa4fd5a97f382014447
Ashley Bedford <42564112+abedford37@users.noreply.github.com>
2019-02-18 03:37:36 -0600

    Flix Pt. 2
    -created the collection view

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Ashley Bedford on 2/18/19.
+//  Copyright © 2019 Ashley Bedford. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
